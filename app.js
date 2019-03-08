8543b4a4f103d7875b9cbb994550b09c84bc5995
TheOTG <kennythesman@gmail.com>
2019-03-08 09:55:59 +0700

    simple

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 require('dotenv').config()
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var quoteRouter = require('./routes/quote');
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use('/', require('./routes/tmdb'))
 
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/quotes', quoteRouter)
-
-module.exports = app;
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
